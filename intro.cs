c18bb6fb0bca9b514b36d4aa447e15ede46377bf
jhildebrand <jacob.hildebrand@birdvilleschools.net>
2015-10-20 23:49:20 -0500

    Add intro and control flow, loops
    add intro.pptx, intro.cs and control flow loops.cs

---
+++ intro.cs
@@ -0,0 +1,35 @@
+//Intro to C#
+
+/*
+Was developed by Microsoft within its .NET initiative and later approved as a standard by Ecma (ECMA-334) and ISO (ISO/IEC 23270:2006). 
+C# is intended to be a simple, modern, general-purpose, object-oriented programming language.[7] Its development team is led by Anders Hejlsberg.
+The most recent version is C# 6.0, which was released on July 20, 2015.[8]
+
+The ECMA standard lists these design goals for C#:[7]
+The C# language is intended to be a simple, modern, general-purpose, object-oriented programming language.
+The language, and implementations thereof, should provide support for software engineering principles such as strong type checking, 
+array bounds checking, detection of attempts to use uninitialized variables, and automatic garbage collection. Software robustness, 
+durability, and programmer productivity are important.
+The language is intended for use in developing software components suitable for deployment in distributed environments.
+Portability is very important for source code and programmers, especially those already familiar with C and C++.
+Support for internationalization is very important.
+C# is intended to be suitable for writing applications for both hosted and embedded systems, ranging from the very large that use 
+sophisticated operating systems, down to the very small having dedicated functions.
+Although C# applications are intended to be economical with regard to memory and processing power requirements, the language was 
+not intended to compete directly on performance and size with C or assembly language.
+
+IDE we will be using is Xamarin Studio.
+*/
+
+using System;
+namespace hello
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine("Hello World!")
+		}
+	}
+}
+
